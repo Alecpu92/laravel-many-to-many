56ecf5d41e697f6e8c3bce80d569cab793a1e740
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-25 17:26:48 +0200

    creato model Tecnhology

--- database/migrations/2023_07_25_152004_create_technologies_table.php
+++ database/migrations/2023_07_25_152004_create_technologies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+
+            $table -> string('name', 32) -> unique();
+            $ABLE _> text('description') -> nullable();
             $table->timestamps();
         });
     }
